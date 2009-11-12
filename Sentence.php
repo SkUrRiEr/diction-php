08275207052ee1968b2f3bdc810b2d35a0d3101f
jcalaby <jcalaby@1e27d619-0aa4-418a-9672-3753e196be1b>
2009-11-12 05:01:07 +0000

    Add copyright notices.
    git-svn-id: https://julian.is-a-geek.org/svn/diction_php@449 1e27d619-0aa4-418a-9672-3753e196be1b

--- Sentence.php
+++ Sentence.php
@@ -1,5 +1,29 @@
 <?php
 
+/* Sentence.php
+ *
+ * Copyright 2009 Julian Calaby <julian.calaby@gmail.com>
+ *
+ * This file is based upon sentence.c from GNU Diction.
+ * 
+ * GNU Diction is GNU software, copyright 1997-2007
+ * Michael Haardt <michael@moria.de>.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 abstract class Sentence {
 	static $abbreviations;
 
